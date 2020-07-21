468d07e17e85efbbd8e5054e5b5f5f814bf724f0
deepmandlik <mandlikdeep20@gmail.com>
2020-07-21 13:43:32 +0530

    Updates

---
+++ precache-manifest.2f2bc5b3a9c0a12ba3d1af6947e13cec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55de1ce9766f0fdcb16c9021889886ec",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "6fe5807f42f2d348d438",
+    "url": "/Tic-Tac-Toe/static/css/main.ddc0bf1d.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/Tic-Tac-Toe/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe5807f42f2d348d438",
+    "url": "/Tic-Tac-Toe/static/js/main.1b0088cc.chunk.js"
+  },
+  {
+    "revision": "340fa9848be46dd5a3e7",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.e29fce77.js"
+  }
+]);
